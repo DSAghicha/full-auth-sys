91bbf67161dc44a82ce1d1267ad7226205d16a6d
DSAghicha <dsaghicha211@gmail.com>
2022-01-23 11:19:25 +0530

    Initial Server Setup

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+require('dotenv').config({path: "./config.env"})
+
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(PORT, () => console.log(`Server is running @ ${PORT}`))
+
+process.on('unhandledRejection',(err) =>{
+    console.error(`Logged Error: ${err}`)
+    server.close(() => process.exit(1))
+})
