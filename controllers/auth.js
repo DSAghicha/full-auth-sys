2a99dfb2c54c60c3c3444579167222669a9b2fe7
DSAghicha <dsaghicha211@gmail.com>
2022-01-25 23:44:06 +0530

    feat: Added Logic for Forget Password & established route for Forgot Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,6 +42,50 @@ exports.login = async(req, res, next) => {
     }
 }
 
+exports.forgotpassword = async(req, res, next) => {
+    const { email } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+
+        if(!user){
+            return next(new ErrorResponse('Email could not ne sent', 404))
+        }
+        const resetToken = user.getResetPasswordToken()
+
+        await user.save()
+
+        const resetURL = `http://localhost:3000/passwordreset/${ resetToken }`
+        const message =
+        `<h1> It seems like you have forgotten your password.</h1>
+        <p>
+            <a href=${resetURL}>Click here</a>
+            to reset your password.`
+
+        try {
+            await sendEmail({
+                to: user.email,
+                subject: "Password Reset",
+                text: message
+            })
+            res.status(200).json({
+                success:true,
+                data: "Email Sent"
+            })
+        } catch (error) {
+            user.getResetPasswordToken = undefined
+            user.resetPasswordExpire = undefined
+
+            await user.save()
+
+            return next(new ErrorResponse("Email could not be sent.", 500))
+        }
+    } catch (error) {
+        next(error)
+        console.error(error);
+    }
+}
+
 const sendToken = (user, statusCode, res) => {
     const token = user.getSignedToken()
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const { register, login } = require('../controllers/auth')
+const { register, login, forgotpassword } = require('../controllers/auth')
 
 const express = require('express')
 const router = express.Router()
@@ -7,5 +7,6 @@ const router = express.Router()
 
 router.route('/register').post(register)
 router.route('/login').post(login)
+router.route('/forgotpassword').post(forgotpassword)
 
 module.exports = router
